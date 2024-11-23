a3202f996c409642bd4183b768bf5004cc897356
86kkd <2973720373@qq.com>
2024-11-23 12:27:27 +0800

    updage

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -9,8 +9,8 @@ return {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
           ["<Leader>gf"] = { "<cmd>Flog<cr>", desc = "Open the git branch graph" },
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 }
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,7 @@
-return
-{
+return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
+  init = function() vim.g.mkdp_filetypes = { "markdown" } end,
   ft = { "markdown" },
 }
